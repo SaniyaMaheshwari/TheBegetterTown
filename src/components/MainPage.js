a53098a0eca2d51deed5c29120c5a57cfb07679c
Vartika Gupta <lci2020024@iiitl.ac.in>
2023-02-25 23:41:54 +0530

    explore page added

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -260,7 +260,7 @@ function MainPage() {
                         <div className="portfolio-info">
                             <h4>App 1</h4>
                             <p>App</p>
-                            <a href="assets/img/portfolio/portfolio-1.jpg" data-gallery="portfolioGallery" className="portfolio-lightbox preview-link" title="App 1"><i className="bx bx-plus"></i></a>
+                            <a href="../img/portfolio/portfolio-1.jpg" data-gallery="portfolioGallery" className="portfolio-lightbox preview-link" title="App 1"><i className="bx bx-plus"></i></a>
                             <a href="portfolio-details.html" className="details-link" title="More Details"><i className="bx bx-link"></i></a>
                         </div>
                     </div>
@@ -270,7 +270,7 @@ function MainPage() {
                         <div className="portfolio-info">
                             <h4>Web 3</h4>
                             <p>Web</p>
-                            <a href="assets/img/portfolio/portfolio-2.jpg" data-gallery="portfolioGallery" className="portfolio-lightbox preview-link" title="Web 3"><i className="bx bx-plus"></i></a>
+                            <a href="../img/portfolio/portfolio-2.jpg" data-gallery="portfolioGallery" className="portfolio-lightbox preview-link" title="Web 3"><i className="bx bx-plus"></i></a>
                             <a href="portfolio-details.html" className="details-link" title="More Details"><i className="bx bx-link"></i></a>
                         </div>
                     </div>
@@ -280,7 +280,7 @@ function MainPage() {
                         <div className="portfolio-info">
                             <h4>App 2</h4>
                             <p>App</p>
-                            <a href="assets/img/portfolio/portfolio-3.jpg" data-gallery="portfolioGallery" className="portfolio-lightbox preview-link" title="App 2"><i className="bx bx-plus"></i></a>
+                            <a href="../img/portfolio/portfolio-3.jpg" data-gallery="portfolioGallery" className="portfolio-lightbox preview-link" title="App 2"><i className="bx bx-plus"></i></a>
                             <a href="portfolio-details.html" className="details-link" title="More Details"><i className="bx bx-link"></i></a>
                         </div>
                     </div>
@@ -290,7 +290,7 @@ function MainPage() {
                         <div className="portfolio-info">
                             <h4>Card 2</h4>
                             <p>Card</p>
-                            <a href="assets/img/portfolio/portfolio-4.jpg" data-gallery="portfolioGallery" className="portfolio-lightbox preview-link" title="Card 2"><i className="bx bx-plus"></i></a>
+                            <a href="../img/portfolio/portfolio-4.jpg" data-gallery="portfolioGallery" className="portfolio-lightbox preview-link" title="Card 2"><i className="bx bx-plus"></i></a>
                             <a href="portfolio-details.html" className="details-link" title="More Details"><i className="bx bx-link"></i></a>
                         </div>
                     </div>
@@ -300,7 +300,7 @@ function MainPage() {
                         <div className="portfolio-info">
                             <h4>Web 2</h4>
                             <p>Web</p>
-                            <a href="assets/img/portfolio/portfolio-5.jpg" data-gallery="portfolioGallery" className="portfolio-lightbox preview-link" title="Web 2"><i className="bx bx-plus"></i></a>
+                            <a href="../img/portfolio/portfolio-5.jpg" data-gallery="portfolioGallery" className="portfolio-lightbox preview-link" title="Web 2"><i className="bx bx-plus"></i></a>
                             <a href="portfolio-details.html" className="details-link" title="More Details"><i className="bx bx-link"></i></a>
                         </div>
                     </div>
@@ -310,7 +310,7 @@ function MainPage() {
                         <div className="portfolio-info">
                             <h4>App 3</h4>
                             <p>App</p>
-                            <a href="assets/img/portfolio/portfolio-6.jpg" data-gallery="portfolioGallery" className="portfolio-lightbox preview-link" title="App 3"><i className="bx bx-plus"></i></a>
+                            <a href="../img/portfolio/portfolio-6.jpg" data-gallery="portfolioGallery" className="portfolio-lightbox preview-link" title="App 3"><i className="bx bx-plus"></i></a>
                             <a href="portfolio-details.html" className="details-link" title="More Details"><i className="bx bx-link"></i></a>
                         </div>
                     </div>
@@ -320,7 +320,7 @@ function MainPage() {
                         <div className="portfolio-info">
                             <h4>Card 1</h4>
                             <p>Card</p>
-                            <a href="assets/img/portfolio/portfolio-7.jpg" data-gallery="portfolioGallery" className="portfolio-lightbox preview-link" title="Card 1"><i className="bx bx-plus"></i></a>
+                            <a href="../img/portfolio/portfolio-7.jpg" data-gallery="portfolioGallery" className="portfolio-lightbox preview-link" title="Card 1"><i className="bx bx-plus"></i></a>
                             <a href="portfolio-details.html" className="details-link" title="More Details"><i className="bx bx-link"></i></a>
                         </div>
                     </div>
@@ -330,7 +330,7 @@ function MainPage() {
                         <div className="portfolio-info">
                             <h4>Card 3</h4>
                             <p>Card</p>
-                            <a href="assets/img/portfolio/portfolio-8.jpg" data-gallery="portfolioGallery" className="portfolio-lightbox preview-link" title="Card 3"><i className="bx bx-plus"></i></a>
+                            <a href="../img/portfolio/portfolio-8.jpg" data-gallery="portfolioGallery" className="portfolio-lightbox preview-link" title="Card 3"><i className="bx bx-plus"></i></a>
                             <a href="portfolio-details.html" className="details-link" title="More Details"><i className="bx bx-link"></i></a>
                         </div>
                     </div>
@@ -340,7 +340,7 @@ function MainPage() {
                         <div className="portfolio-info">
                             <h4>Web 3</h4>
                             <p>Web</p>
-                            <a href="assets/img/portfolio/portfolio-9.jpg" data-gallery="portfolioGallery" className="portfolio-lightbox preview-link" title="Web 3"><i className="bx bx-plus"></i></a>
+                            <a href="../img/portfolio/portfolio-9.jpg" data-gallery="portfolioGallery" className="portfolio-lightbox preview-link" title="Web 3"><i className="bx bx-plus"></i></a>
                             <a href="portfolio-details.html" className="details-link" title="More Details"><i className="bx bx-link"></i></a>
                         </div>
                     </div>
